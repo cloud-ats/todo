e3103a5e882813ed6d379266bc51fe62227bc205
tuanhq <hoangquoctuan1985@gmail.com>
2014-12-03 16:10:40 +0700

    modifield backend

---
+++ app/models/generic/GenericDTO.java
@@ -0,0 +1,13 @@
+package models.generic;
+
+import com.mongodb.BasicDBObject;
+
+public abstract class GenericDTO extends BasicDBObject {
+  public GenericDTO(){
+	
+  }
+  public void from(BasicDBObject doc){
+	  
+  }
+
+}
